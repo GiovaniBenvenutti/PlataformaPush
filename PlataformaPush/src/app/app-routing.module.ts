fb6fc0f43fc80ed648c2e29488c7902bc3eaa2b2
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2024-01-05 14:55:04 -0300

    litlle improves

--- PlataformaPush/src/app/app-routing.module.ts
+++ PlataformaPush/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { NotifierComponent } from './notifier/notifier.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PainelComponent } from './painel/painel.component';
 
 const routes: Routes = [
 
   { path : 'auth', component: AuthComponent },
+  { path : 'painel', component: PainelComponent },  
   { path : 'notifier', component: NotifierComponent },  
   { path : 'dashboard', component: DashboardComponent }, 
   { path : '', redirectTo: 'auth', pathMatch: 'full' }
