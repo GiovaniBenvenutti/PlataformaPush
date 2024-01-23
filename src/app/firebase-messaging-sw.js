3feda40b2426ab4a646f8cfe346175b46bc5eb46
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2024-01-23 12:44:02 -0300

    retest

---
+++ src/app/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+
+importScripts("https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging-compat.js");
+firebase.initializeApp({
+    apiKey: "AIzaSyD9efTQuEPBKpMalL7xLMfnoxFeYJ24Dws",
+    authDomain: "push-notifier-737e2.firebaseapp.com",
+    projectId: "push-notifier-737e2",
+    storageBucket: "push-notifier-737e2.appspot.com",
+    messagingSenderId: "1086353491958",
+    appId: "1:1086353491958:web:0458396c584303ed43e9d7",
+    measurementId: "G-RHKJJFMESH"
+});
+const messaging = firebase.messaging();
