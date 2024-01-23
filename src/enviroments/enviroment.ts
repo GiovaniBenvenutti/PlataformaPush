0857d50742101a8631a0d756d65e3fd3b133c8de
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2024-01-23 12:59:15 -0300

    progree?

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyD9efTQuEPBKpMalL7xLMfnoxFeYJ24Dws",
+        authDomain: "push-notifier-737e2.firebaseapp.com",
+        projectId: "push-notifier-737e2",
+        storageBucket: "push-notifier-737e2.appspot.com",
+        messagingSenderId: "1086353491958",
+        appId: "1:1086353491958:web:0458396c584303ed43e9d7",
+        measurementId: "G-RHKJJFMESH",
+        vapidKey: "BNZUbV-u7WM3UyA4FHySD0ZCf9dpgdXqu9QshcE85kHDIpnpaDcIi_r2UXRaGqWI1hcdTtX2p69WTstqSF2Q0SQ"
+    },
+  };
